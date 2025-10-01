f2ad6e67b43b1f86fce8a056dabb1d556aa8bcbf
Parsa Yazdani (Quix) <parsa@yazdani.au>
2025-10-01 22:09:59 +1000

    Catch DTS-HDMA

--- src/handlers.js
+++ src/handlers.js
@@ -105,7 +105,7 @@ exports.addDefaults = /** @type Parser */ parser => {
     parser.addHandler("color", /\b(?:DV|DoVi)\b/i, { value: "DV" });
 
     // Audio
-    parser.addHandler("audio", /DTS-HD[\s-.]MA/, { value: "dts-hd-ma" });
+    parser.addHandler("audio", /DTS-HD[\s-.]?MA/, { value: "dts-hd-ma" });
     parser.addHandler("audio", /DTS-ES/, { type: "lowercase" });
     parser.addHandler("audio", /DTS(?:[- ]?HD)?/, { type: "lowercase", skipIfAlreadyFound: true });
     parser.addHandler("audio", /\bATMOS\b/i, { type: "lowercase" });
