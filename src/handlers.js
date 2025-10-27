4507be1b37d15b9c7e58755913cfff9a80fc0766
Parsa Yazdani <parsa@yazdani.au>
2025-10-27 21:33:39 +1100

    hdr10+ handler

--- src/handlers.js
+++ src/handlers.js
@@ -124,7 +124,8 @@ exports.addDefaults = /** @type Parser */ parser => {
     });
 
     // Color
-    parser.addHandler("color", /\bHDR(?:10)?\b/i, { value: "HDR" });
+    parser.addHandler("color", /\bHDR(?:10)?(?!\+)\b/i, { value: "HDR" });
+    parser.addHandler("color", /\bHDR10\+[\b\s]/i, { value: "HDR10+" });
     parser.addHandler("color", /\bSDR\b/i, { type: "uppercase" });
     parser.addHandler("color", /\b(?:DV|DoVi|Dolby\sVision)\b/i, { value: "DV" });
 
