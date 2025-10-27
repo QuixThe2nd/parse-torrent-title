ebf1ebc210e99ea7c67a89755a4a0f6a85d9b9f0
Parsa Yazdani <parsa@yazdani.au>
2025-10-27 22:23:54 +1100

    NOW service

--- src/handlers.js
+++ src/handlers.js
@@ -105,6 +105,7 @@ exports.addDefaults = /** @type Parser */ parser => {
     parser.addHandler("service", /\bHULU\b/i, { type: "uppercase" });
     parser.addHandler("service", /\bH?MAX\b/, { value: "HMAX" });
     parser.addHandler("service", /\b(?<!DTS-HD[\s\-\.])MA\b/i, { type: "uppercase" });
+    parser.addHandler("service", /\bNOW\b/, { type: "uppercase" });
     parser.addHandler("service", /\bMFSL\b/i, { type: "uppercase" });
     parser.addHandler("service", /\b(NFLX|NF|Netflix)\b/i, { value: "NFLX" });
     parser.addHandler("service", /\biT(?:unes)\b/, { value: "iT" });
