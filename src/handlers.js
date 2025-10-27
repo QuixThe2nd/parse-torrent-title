d5938c96f2a1082dde8a2789f8e2b816f6d83abf
Parsa Yazdani <parsa@yazdani.au>
2025-10-27 22:16:32 +1100

    catch HDR10+.

--- src/handlers.js
+++ src/handlers.js
@@ -125,7 +125,7 @@ exports.addDefaults = /** @type Parser */ parser => {
 
     // Color
     parser.addHandler("color", /\bHDR(?:10)?(?!\+|plus)\b/i, { value: "HDR" });
-    parser.addHandler("color", /\bHDR10(?:\+|plus)[\b\s]/i, { value: "HDR10+" });
+    parser.addHandler("color", /\bHDR10(?:\+|plus)[\b\s.]/i, { value: "HDR10+" });
     parser.addHandler("color", /\bSDR\b/i, { type: "uppercase" });
     parser.addHandler("color", /\b(?:DV|DoVi|Dolby\sVision)\b/i, { value: "DV" });
 
